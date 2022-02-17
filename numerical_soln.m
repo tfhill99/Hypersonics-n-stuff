237ae73d98378e18bdd2fbbaf1df62bf2e9133ed
Dayan Mitchell <87230995+dayanmitchell@users.noreply.github.com>
2022-02-17 12:08:28 -0500

    Update numerical_soln.m
    Added Mach number and test matrix

--- numerical_soln.m
+++ numerical_soln.m
@@ -56,7 +56,7 @@ for i = 1:length(V)
     V{i} = V{i}(1:index);
     gamma{i} = gamma{i}(1:index);
     Z{i} = Z{i}(1:index);
-    
+
 end
 %% Plotting V(z)
 figure(1)
@@ -126,6 +126,25 @@ xlabel('Dynamic Pressure (kg/ms^2)');
 ylabel('Altitude (m)');
 title('Altitude v/s Dynamic Pressure');
 
+%% Calculating Mach
+[T, a, P, rho] = atmoscoesa(Z_analytical);
+Mach = V_analytical./a;
+figure(5)
+plot(Mach, Z_analytical, '-.')
+legend(' ');
+xlabel('Mach Number');
+ylabel('Altitude (m)');
+title('Altitude v/s Mach Number');
+
+v_table = [V_analytical(1),V_analytical(100),V_analytical(200),V_analytical(300),V_analytical(400),V_analytical(500),V_analytical(600),V_analytical(700),V_analytical(800),V_analytical(900),V_analytical(1000)].';
+t_table = [T(1),T(100),T(200),T(300),T(400),T(500),T(600),T(700),T(800),T(900),T(1000)].';
+rho_table = [rho(1),rho(100),rho(200),rho(300),rho(400),rho(500),rho(600),rho(700),rho(800),rho(900),rho(1000)].';
+M_table = [Mach(1),Mach(100),Mach(200),Mach(300),Mach(400),Mach(500),Mach(600),Mach(700),Mach(800),Mach(900),Mach(1000)].';
+z_table = [Z_analytical(1),Z_analytical(100),Z_analytical(200),Z_analytical(300),Z_analytical(400),Z_analytical(500),Z_analytical(600),Z_analytical(700),Z_analytical(800),Z_analytical(900),Z_analytical(1000)].';
+
+table(v_table,t_table,rho_table,M_table,z_table);
+
+
 %% Function
 % Use V, gamma, z
 function dydt = odefcn(t,y,beta,k_d,g,R_E,rho_0)
@@ -133,4 +152,4 @@ function dydt = odefcn(t,y,beta,k_d,g,R_E,rho_0)
   dydt(1) = (-0.5*rho_0*exp(-1*beta*y(3)) * y(1)^2)/k_d - g*(1 - (y(1)^2)/(R_E*g*(1 + y(3)/R_E)))*sin(y(2));
   dydt(2) = (-1*g*(1 - (y(1)^2)/(R_E*g*(1+y(3)/R_E)))*cos(y(2)))/y(1);
   dydt(3) = y(1) * sin(y(2)); 
-end
+end 
