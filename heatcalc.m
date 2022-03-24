dc807d99dfc143f1d2323b0fbd9ec62d119d362b
pranav270-create <70440294+pranav270-create@users.noreply.github.com>
2022-03-24 12:57:08 -0400

    Update heatcalc.m

--- heatcalc.m
+++ heatcalc.m
@@ -50,36 +50,67 @@ file = 'CAD_capsule_3.stl';
 
 %% Calculate Stagnation Pressure along Flight Path
 
-R_n = 2.72; %m
+R_n = 0.272; %m
 c_p = 1.00; % kJ/kg
-T_e = [800 900 1000 1100 1200]';
+N = 0.5;
+M = 3;
+r = 0.71^0.5; %laminar flow where 0.71 is Prandtl as r = Pr^n
+T_e = [100 500 1000 1500 2000]; %K
 q_w = {};
 
 for i = 1:5
+    % Fix wall temperature
     T_w = T_e(i);
     % Tauber Menees
-    N = 0.5;
-    M = 3;
-    r = 0.71^0.5; %laminar flow where 0.71 is Prandtl as r = Pr^n
     T_aw = T_w + r*V.^2/(2*c_p);
     C = 1.29*10^-4*R_n^-0.5*(1-T_w/T_aw);
     q_w{i} = C*rho_traj.^N.*V.^M;
 end
 
-%% Plotting q
+%% Calculate Blackbody Radiation along Traj
+
+eps = 0.7;
+sigma = 5.6695 * 10^-8; % Stefan Boltzmann [W/m^2/K^4]
+q_r = {};
+for i = 1:5
+    % Fix wall temperature
+    T_w = T_e(i);
+    % Tauber Menees
+    q_r{i} = eps*sigma*T_w^4 * ones(length(Z),1);
+end
+
+%% Correct q_total
+
+q_total = {};
+for i = 1:5
+    q_total{i} = q_w{i} - q_r{i};
+end
+
+%% Plotting q_w and q_total
 
 figure(2)
 plot(Z, q_w{1})
 hold on
-plot(Z, q_w{2})
-hold on
-plot(Z, q_w{3})
-hold on
-plot(Z, q_w{4})
-hold on
-plot(Z, q_w{5})
-hold on
-title('Stagnation Heating at Various Temperatures along Trajectory')
+plot(Z, q_total{1})
+title('Stagnation Flux along Trajectory, T_w = 800 K')
 xlabel('Altitude (m)')
-ylabel('q_w')
+ylabel('q_w (W/cm^2)')
+legend('Without Blackbody Radiation','With Blackbody Radiation')
+
+%% Plotting q_total across T
+
+figure(3)
+plot(Z, q_total{1})
+hold on
+plot(Z, q_total{2})
+hold on
+plot(Z, q_total{3})
+hold on
+plot(Z, q_total{4})
+hold on
+plot(Z, q_total{5})
+hold on
+title('Stagnation Flux at Various Temperatures along Trajectory')
+xlabel('Altitude (m)')
+ylabel('q_w (W/cm^2)')
 legend('800','900','1000','1100','1200')
