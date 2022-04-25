f0aaf5cb2c05718068ab14358fa1e2b40ef0c2ea
tfhill99 <99200167+tfhill99@users.noreply.github.com>
2022-04-25 12:19:57 -0400

    Update TPS_materials.m

--- TPS_materials.m
+++ TPS_materials.m
@@ -1,6 +1,34 @@
 clear all 
 close all
 
+max_weight = 8.5; % kg
+max_surface_temp = 70; % °C
+
+%% Rigid TPS Materials
+
+rho_FW12 = 2900; % kg/m^3
+rho_Rescor310M = 800; % kg/m^3
+rho_Rescor311 = 800; % kg/m^3
+rho_Intek1120 = 6.4; % kg/m^3
+
+c_FW12 = 1500; % J/K*kg
+c_Rescor310M = 1457; % J/K*kg
+c_Rescor311 = 1457; % J/K*kg
+c_Intek1120 = 1009; % J/K*kg
+
+lambda_FW12 = 3.30; % W/(K*m) Up to 2.2 at 1100 °C
+lambda_Rescor310M = 0.18; % W/(K*m) at 1650 °C
+lambda_Rescor311 = 0.18; % W/(K*m) at 1430 °C
+lambda_Intek1120 = 0.046; % W/(K*m) at any °C
+
+max_temp_FW12 = 1700; % °C
+max_temp_Rescor310M = 1650; % °C
+max_temp_Rescor311 = 1430; % °C
+max_temp_Intek1120 = 300; % °C
+
+
+%% Flexible TPS Materials
+
 rho_nextel = 2700; % kg/m^3
 rho_sigratherm = 92; % kg/m^3
 rho_pyrogel = 92; % kg/m^3
