57d78df84695e3083f92885522cb25ba1d14c277
pranav270-create <70440294+pranav270-create@users.noreply.github.com>
2022-04-26 15:13:09 -0400

    fixing integration

--- Heat_trasnfer.m
+++ Heat_trasnfer.m
@@ -1,6 +1,5 @@
 %qw = q_w_stag_TM(1:12000); 
 n = 100; %discretization
-%total_time = time(12000); %steps of solving
 
 thickness = [0.001, 0.001, 0.001, 0.001, 0.001, 0.0005, 0.0005];
 total_thickness = sum(thickness); %m
@@ -13,7 +12,7 @@ sigma = 5.6695e-8;
 
 % set this to q_w_stag_TM if looking at stagnation point
 % set this to q_w_TM if looking at point midway on cone
-qw = q_w_TM; 
+qw = q_w_stag_TM(1:12000); 
 
 cumthick = 0;
 cum_thickness = ones(7,1);
@@ -29,17 +28,23 @@ for i= 1:length(thickness)
     indices(i) = sum(sizes);
 end
 
-% sigma = 5.6695e-8; 
-% eps = 0.9; 
+%% Setup integration parameters 
 
 dx = total_thickness/n;
-dts = 0.7*0.5*dx^2./alphas; %5e-6
+dt = 0.9*0.5*dx^2./max(alphas); 
+
+total_time = time(12000);
+integration_len = round(total_time/dt);
+integration_array = (0:integration_len-1)*dt;
+%time_int = linspace(0, total_time, 10000);
+
+%time_integrate = interp1(total_time, time(1:12000)) 
 
 %% Setup matrixes
 
 A_total = [];
 for i = 1:length(sizes)
-    b = ones(sizes(i),1)*1/2*alphas(i)*dts(i)/(dx^2);
+    b = ones(sizes(i),1)*1/2*alphas(i)*dt/(dx^2);
     A = spdiags([-b 1+2*b -b],-1:1, sizes(i),sizes(i));
     if i == 1
         A(1,1) = -1;
@@ -52,7 +57,7 @@ for i = 1:length(sizes)
 end
 
 for i= 1:length(sizes)-1
-    b  = 1/2*alphas(i)*dts(i)/(dx^2);
+    b  = 1/2*alphas(i)*dt/(dx^2);
     A_total(indices(i),indices(i)+1) = -b;
     A_total(indices(i)+1,indices(i)) = -b;
 end
@@ -75,21 +80,20 @@ for idx=1:12000
             q(i) = -dx/lambdas(7)*sigma*eps*T(n)^4;
         else
             if i <= indices(1)
-                b_mat = 0.5*alphas(1)*dts(1)/(dx^2);
+                b_mat = 0.5*alphas(1)*dt/(dx^2);
             elseif (indices(1) < i) && (i <= indices(2))
-                b_mat = 0.5*alphas(2)*dts(2)/(dx^2);
+                b_mat = 0.5*alphas(2)*dt/(dx^2);
             elseif (indices(2) < i) && (i <= indices(3))
-                b_mat = 0.5*alphas(3)*dts(3)/(dx^2);
+                b_mat = 0.5*alphas(3)*dt/(dx^2);
             elseif (indices(3) < i) && (i <= indices(4))
-                b_mat = 0.5*alphas(4)*dts(4)/(dx^2);
+                b_mat = 0.5*alphas(4)*dt/(dx^2);
             elseif (indices(4) < i) && (i <= indices(5))
-                b_mat = 0.5*alphas(5)*dts(5)/(dx^2);
+                b_mat = 0.5*alphas(5)*dt/(dx^2);
             elseif (indices(5) < i) && (i <= indices(6))
-                b_mat = 0.5*alphas(6)*dts(6)/(dx^2);
+                b_mat = 0.5*alphas(6)*dt/(dx^2);
             else
-                b_mat = 0.5*alphas(7)*dts(7)/(dx^2);
+                b_mat = 0.5*alphas(7)*dt/(dx^2);
             end
-            disp(i)
             q(i) = b_mat*T(i-1)+(1-2*b_mat)*T(i)+b_mat*T(i+1);
         end
     end
@@ -114,7 +118,15 @@ hold on
 plot(T_23)
 hold on
 plot(baseline, '--')
-legend('front', 'back','1/3','2/3','limit')
+legend('front', '6 mm','2 mm','4 mm','70 C')
+set(gcf,'color','w');
+
+%%
+figure(2)
+plot(q_w_TM(1:12000))
+hold on
+plot(q_w_stag_TM(1:12000))
+legend('MidPoint', 'Cone')
 set(gcf,'color','w');
 
 
